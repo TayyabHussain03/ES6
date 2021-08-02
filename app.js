438cfaa64cb034f25e5c06966c2fe0fb636844d6
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-08-02 22:04:42 +0500

    template strings added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+// **********Template Strings********** 
 
+// Old JS
+
+// let a = "tayyab"
+// let b = "hussain"
+// console.log("my name is" + a + "" + b)
+
+// In ES6
+
+// let a = "tayyab" ;
+// let b = "hussain";
+// console.log(`my name is ${a} ${b}`)
