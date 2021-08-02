b0e49550e7dbb862bccf472e68156edf1a0703af
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-08-02 22:10:18 +0500

    destructuring array added

--- app.js
+++ app.js
@@ -11,3 +11,20 @@
 // let a = "tayyab" ;
 // let b = "hussain";
 // console.log(`my name is ${a} ${b}`)
+
+// **********Destructuring Array**********
+
+// Old JS
+
+// let user = ['tayyab', '18', 'male']
+// let name = user[0]
+// let age = user[1]
+// let gender = user[2]
+
+// console.log(age)
+
+// In ES6
+
+// let user = ['tayyab', '18', 'male']
+// let[name,age,gender]=user
+// console.log(name)
